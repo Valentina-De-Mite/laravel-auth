05c0b5a0a502eba1a82e142dc3a5c56182f8466f
Valentina De Mite <valesakura97@gmail.com>
2023-11-08 17:42:25 +0100

    added migration

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/*Admin route*/
+Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
