4041e6dda1233135d65de12ec515ac44aab37031
Valentina De Mite <valesakura97@gmail.com>
2023-11-13 17:10:40 +0100

    added ralations has many and belongs to

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,13 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
 
-    protected $fillable = ['title', 'description', 'cover_image'];
+    protected $fillable = ['title', 'description', 'cover_image', 'content'];
+
+
+    /**
+     * Get all the category that Project
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo;
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(category::class);
+    }
 }
+
+
 
 
